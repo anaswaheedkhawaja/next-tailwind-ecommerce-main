7e12a3ce08720a998492a78c1aeb031b375ae3ce
Khawaja Anas Waheed <anaswaheedkhawaja@gmail.com>
2023-08-06 11:37:27 +0500

    Initial commit

---
+++ src/pages/auth/login.js
@@ -0,0 +1,8 @@
+import Form from "@/components/auth/form";
+
+export default function SignIn () {
+    const onSubmit = () => {
+
+    };
+    return <Form signin={true} onFormSubmit={onSubmit} />
+};
