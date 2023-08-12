4f053af4cabd6027f55a7cd047d10138322619d3
Khawaja Anas Waheed <anaswaheedkhawaja@gmail.com>
2023-08-13 02:23:29 +0500

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -250,12 +250,12 @@ export default function Example() {
 
                 <div className="space-y-6 border-t border-gray-200 px-4 py-6">
                   <div className="flow-root">
-                    <a href="auth/signin.js" className="-m-2 block p-2 font-medium text-gray-900">
+                    <a href="/auth/login" className="-m-2 block p-2 font-medium text-gray-900">
                       Sign in
                     </a>
                   </div>
                   <div className="flow-root">
-                    <a href="/auth/signup.js" className="-m-2 block p-2 font-medium text-gray-900">
+                    <a href="/auth/signup" className="-m-2 block p-2 font-medium text-gray-900">
                       Create account
                     </a>
                   </div>
@@ -415,11 +415,11 @@ export default function Example() {
 
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                  <a href="/auth/login" className="text-sm font-medium text-gray-700 hover:text-gray-800">
                     Sign in
                   </a>
                   <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                  <a href="/auth" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                  <a href="/auth/signup" className="text-sm font-medium text-gray-700 hover:text-gray-800">
                     Create account
                   </a>
                 </div>
---
+++ src/pages/profile.js
@@ -0,0 +1,32 @@
+import { redirect } from "next/dist/server/api-utils";
+
+export default function SignUp () {
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Your Profile Page
+          </h2>
+        </div>
+        </div>
+    )
+};
+
+
+export async function getServersSideProps ({req}) {
+    const session = await getSession({req});
+    if(!session) {
+      return {
+        redirect: {
+          destination: "/auth/login",
+          permanent: false
+        }
+      }
+    }
+
+    return {
+      props: {
+        session
+      }
+    }
+}
